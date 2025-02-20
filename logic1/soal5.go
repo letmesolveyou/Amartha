7aa395555d63125691ce664bc439fc8103540906
letmesolveyou <safia.amita@gmail.com>
2025-02-20 10:06:17 +0700

    Mengerjakan logic 1

--- logic1/soal5.go
+++ logic1/soal5.go
@@ -1,13 +1,13 @@
-package main
+package logic1
 
 import "fmt"
 
-func main() {
+func Soal5() {
 	var n = 10
 	var result [10]int
 
 	for i := 1; i <= n; i++ {
-		result[i-1] = 21 - 2*i
+		result[i-1] = 22 - 2*i
 	}
 
 	fmt.Println(result)
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "belajar-golang-dasar/logic1"
+
+func main() {
+	logic1.Soal6()
+}
